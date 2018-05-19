8c1a457cc896e086e4647df11883fe4ce94aa6f2
jhaye <jhaye@gmx.de>
2018-05-19 16:36:47 +0200

    Adds 'C-API equivalent' section to all functions in OlmAccount

--- src/account.rs
+++ src/account.rs
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+//! This module wraps around all functions following the pattern `olm_account_*`.
+
 use errors;
 use errors::OlmAccountError;
 use olm_sys;
@@ -34,8 +36,10 @@ impl OlmAccount {
     /// and the Curve25519 identity key pair are generated. For more information see
     /// [here](https://matrix.org/docs/guides/e2e_implementation.html#keys-used-in-end-to-end-encryption).
     ///
-    /// # Panics
+    /// # C-API equivalent
+    /// `olm_create_account`
     ///
+    /// # Panics
     /// * `NOT_ENOUGH_RANDOM` for OlmAccount's creation
     ///
     pub fn new() -> Self {
@@ -72,8 +76,10 @@ impl OlmAccount {
 
     /// Returns the account's public identity keys already formatted as JSON and BASE64.
     ///
-    /// # Panics
+    /// # C-API equivalent
+    /// `olm_account_identity_keys`
     ///
+    /// # Panics
     /// * `OUTPUT_BUFFER_TOO_SMALL` for supplied identity keys buffer
     ///
     pub fn identity_keys(&mut self) -> String {
@@ -132,10 +138,12 @@ impl OlmAccount {
         }
     }
 
-    /// Returns the signature of the supplied byte slice
+    /// Returns the signature of the supplied byte slice.
+    ///
+    /// # C-API equivalent
+    /// `olm_account_sign`
     ///
     /// # Panics
-    ///
     /// * `OUTPUT_BUFFER_TOO_SMALL` for supplied signature buffer
     ///
     pub fn sign_bytes(&self, input_buf: &mut [u8]) -> String {
@@ -174,20 +182,26 @@ impl OlmAccount {
     }
 
     /// Convenience function that converts the UTF-8 message
-    /// to bytes and then calls sign_bytes(), returning its output.
+    /// to bytes and then calls `sign_bytes()`, returning its output.
     pub fn sign_utf8_msg(&self, msg: &mut str) -> String {
         unsafe { self.sign_bytes(msg.as_bytes_mut()) }
     }
 
-    /// Maximum number of one time keys that this OlmAccount can hold.
+    /// Maximum number of one time keys that this OlmAccount can currently hold.
+    ///
+    /// # C-API equivalent
+    /// `olm_account_max_number_of_one_time_keys`
+    ///
     pub fn max_number_of_one_time_keys(&self) -> usize {
         unsafe { olm_sys::olm_account_max_number_of_one_time_keys(self.olm_account_ptr) }
     }
 
     /// Generates the supplied number of one time keys.
     ///
-    /// # Panics
+    /// # C-API equivalent
+    /// `olm_account_generate_one_time_keys`
     ///
+    /// # Panics
     /// * `NOT_ENOUGH_RANDOM` for the creation of one time keys
     ///
     pub fn generate_one_time_keys(&mut self, number_of_keys: usize) {
@@ -230,8 +244,10 @@ impl OlmAccount {
 
     /// Gets the OlmAccount's one time keys formatted as JSON.
     ///
-    /// # Panics
+    /// # C-API equivalent
+    /// `olm_account_one_time_keys`
     ///
+    /// # Panics
     /// * `OUTPUT_BUFFER_TOO_SMALL` for supplied one time keys buffer
     ///
     pub fn one_time_keys(&mut self) -> String {
@@ -269,6 +285,10 @@ impl OlmAccount {
     }
 
     /// Mark the current set of keys as published.
+    ///
+    /// # C-API equivalent
+    /// `olm_account_mark_keys_as_published`
+    ///
     pub fn mark_keys_as_published(&mut self) {
         unsafe {
             olm_sys::olm_account_mark_keys_as_published(self.olm_account_ptr);
