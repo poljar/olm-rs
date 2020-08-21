af483ef1715c3071fcd6da4958ee2216288b1e14
Johannes Hayeß <jhaye@mailbox.org>
2020-08-21 12:32:59 +0200

    Also test for after using set_their_public_key

--- fuzz/fuzz_targets/olm_sas1.rs
+++ fuzz/fuzz_targets/olm_sas1.rs
@@ -4,12 +4,13 @@ use libfuzzer_sys::fuzz_target;
 use olm_rs::sas;
 
 fuzz_target!(|data: String| {
-    {  // add scope to test dropping OlmSas
-        let s = sas::OlmSas::new();
+    {
+        // add scope to test dropping OlmSas
+        let s1 = sas::OlmSas::new();
+        let mut s2 = sas::OlmSas::new();
+        assert!(s2.set_their_public_key(s1.public_key()).is_ok());
 
-        let _ = match s.generate_bytes(&data, data.len()) {
-            Ok(_) => (),
-            Err(_) => (),
-        };
+        assert!(s1.generate_bytes(&data, data.len()).is_err());
+        assert!(s2.generate_bytes(&data, data.len()).is_ok());
     }
 });
